80f7f2f7aa75ac80223f847d981f4a60fa585590
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-11-08 17:54:15 +0100

    add: ex2 array

---
+++ snack2/js/script.js
@@ -0,0 +1,19 @@
+const teams = [
+    {
+        name: "Inter",
+        points: 0,
+        injuries: 0
+    },
+
+    {
+        name: "Juve",
+        points: 0,
+        injuries: 0
+    },
+
+    {
+        name: "Milan",
+        points: 0,
+        injuries: 0
+    },
+]
