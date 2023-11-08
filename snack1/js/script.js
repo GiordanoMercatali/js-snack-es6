85f391945eb8415828425448f19600c4e981d08b
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-11-08 15:39:39 +0100

    add: array

---
+++ snack1/js/script.js
@@ -0,0 +1,23 @@
+const bikeArray = [
+    {
+        name: "bike1",
+        weight: 10
+    },
+
+    {
+        name: "bike2",
+        weight: 8.1
+    },
+
+    {
+        name: "bike3",
+        weight: 8
+    },
+
+    {
+        name: "bike4",
+        weight: 12
+    },
+];
+
+const [firstBike, secondBike, thirdBike, fourthBike] = bikeArray;
